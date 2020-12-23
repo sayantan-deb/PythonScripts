c1157834f931104ef4d7383d1c6b6e8fb8c620f1
sayantan-deb <65012528+sayantan-deb@users.noreply.github.com>
2020-12-23 19:20:24 +0530

    Create Volume_of_sphere.py

---
+++ Python-ProblemSet/Volume_of_sphere.py
@@ -0,0 +1,8 @@
+# 4)Practice using the Python interpreter as a calculator:
+#
+# a) The volume of a sphere with radius r is 4/3pr3. What is the volume of a sphere with radius 5?
+# Hint: 392.7 is wrong!
+
+r=int(input("enter the radius"))
+sphere=(4/3)*3.14*(r^3)
+print(sphere)
